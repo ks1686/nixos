c6bb980f93310952145756193657edc201b3aa18
Karim Smires <k.smires1686@gmail.com>
2025-05-11 22:25:43 -0400

    Update config.fish

--- .config/config.fish
+++ .config/config.fish
@@ -5,8 +5,8 @@ end
 
 # System and Configuration Management
 alias nixconf 'env VISUAL="code --wait" sudoedit /etc/nixos/configuration.nix'
-alias nixrebuild 'sudo nixos-rebuild switch --upgrade'
-alias clean "sudo nix-collect-garbage -d; sudo /run/current-system/bin/switch-to-configuration boot"
+alias nixbuild 'sudo nixos-rebuild switch --upgrade'
+alias nixclean "sudo nix-collect-garbage -d; sudo /run/current-system/bin/switch-to-configuration boot"
 
 # Navigation and File Management
 alias conf "code /home/ks1686/.config/"
